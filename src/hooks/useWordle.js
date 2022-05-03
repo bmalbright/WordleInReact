b784c198bf484c36fe1668f32db1a2b262b64e2b
bmalbright <brianalbright@utexas.edu>
2022-05-03 01:38:18 -0500

    added row.js and grid.js to components folder.

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 const useWordle = () => {
   const [turn, setTurn] = useState(0);
   const [currentGuess, setCurrentGuess] = useState("");
-  const [guesses, setGuesses] = useState([]); //each guess is an array
+  const [guesses, setGuesses] = useState([...ArrayBuffer(6)]); //each guess is an array
   const [history, setHistory] = useState([]); //each guess is a string
   const [isCorrect, setIsCorrect] = useState(false);
 
@@ -36,16 +36,32 @@ const useWordle = () => {
   // add a new guess to the guesses state
   // update the isCorrect state if the guess is correct
   // add one to the turn state
-  const addNewGuess = () => {};
+  const addNewGuess = (formattedGuess) => {
+      if (currentGuess === solution) {
+          setIsCorrect(true)
+      }
+      setGuesses((prevGuesses) => {
+        let newGuesses = [...prevGuesses]
+        newGuesses[turn] = formattedGuess
+        return newGuesses
+      })
+      setHistory(() => {
+          return [...prevHistory, currentGuess]
+      })
+      setTurn(() => {
+          return prevTurn +1
+      })
+      setCurrentGuess('')
+  };
 
   // handle keyup event & track current guess
   // if user presses enter, add the new guess
   const handleKeyup = ({ key }) => {
     if (key === "Enter") {
       // only add guess if turn is less than five
-      if (turn > 6) {
+      if (turn > 5) {
         console.log(
-          "You have used your guesses. You are the weakest link. Goodbye."
+          "You have used your guesses."
         );
         return;
       }
@@ -59,7 +75,7 @@ const useWordle = () => {
         console.log("Guesses must be five letters in length.");
       }
       const formatted = formatGuess();
-      console.log(formatted)
+      addNewGuess(formatted)
     }
 
     if (key === "Backspace") {
