627d1ebfb238de1a9da523ab59f15cedb293816b
bmalbright <brianalbright@utexas.edu>
2022-06-01 20:10:21 -0500

    added the keypad.js component. updated wordle.js and the css file.

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import useWordle from "../hooks/useWordle";
 import Grid from "./Grid";
+import Keypad from "./Keypad";
 
 export default function Wordle({ solution }) {
-  const { currentGuess, handleKeyup, guesses, isCorrect, turn } = useWordle(solution);
+  const { currentGuess, handleKeyup, guesses, isCorrect, turn } =
+    useWordle(solution);
 
   useEffect(() => {
     window.addEventListener("keyup", handleKeyup);
@@ -12,14 +14,15 @@ export default function Wordle({ solution }) {
   }, [handleKeyup]);
 
   useEffect(() => {
-      console.log(guesses, turn, isCorrect)
-  }, [guesses, turn, isCorrect])
+    console.log(guesses, turn, isCorrect);
+  }, [guesses, turn, isCorrect]);
 
   return (
     <div>
       <div>solution - {solution}</div>
       <div>Current Guess - {currentGuess}</div>
-      <Grid currentGuess={currentGuess} guesses={guesses} turn={turn}/>
+      <Grid guesses={guesses} currentGuess={currentGuess} turn={turn} />
+      <Keypad />
     </div>
   );
 }
