e1899d071bc95dbe7be7cdf99f56054340a59aa5
bmalbright <brianalbright@utexas.edu>
2022-06-11 18:53:08 -0500

    database is good. app is working. disable the reset button until after first deployment.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -10,9 +10,9 @@ export default function Modal({ isCorrect, turn, solution }) {
                 <h1> Congratulations! You Win!</h1>
                 <p className='solution'> {solution} </p>
                 <p> You found the solution in {turn} guesses. </p>
-                <a href="http://localhost:3000/">
+                {/* <a href="http://localhost:3000/">
                 <button type="submit"> PLAY AGAIN?</button>
-                </a>
+                </a> */}
             </div>
         )};
 
@@ -22,9 +22,9 @@ export default function Modal({ isCorrect, turn, solution }) {
                 <h1> You have used up all of your guesses</h1>
                 <p className='solution'> The answer was {solution} </p>
                 <p> Better luck next time. </p>
-                <a href="http://localhost:3000/">
+                {/* <a href="http://localhost:3000/">
                 <button type="submit"> PLAY AGAIN?</button>
-                </a>
+                </a> */}
             </div>
         )};
     </div>
