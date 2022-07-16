15c3c8d2e5742f87a0340cdf0e9a58a997df9dbd
Brian Albright <brianalbright@utexas.edu>
2022-07-15 19:29:38 -0500

    added concurrently to the package json. Changed start line on package json. Played around with styling. Added index.css page with floating orbs background.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,29 +2,30 @@ import React from 'react';
 
 export default function Modal({ isCorrect, turn, solution }) {
 
+    // function replayWordle() {
+    //     window.location.reload(false)
+    // }
+
   return (
     <div className='modal'>
         {/* modal for if they win */}
         {isCorrect && (
             <div>
-                <h1> Congratulations! You Win!</h1>
+                <h2> Congratulations! You Win!</h2>
+                <br/>
                 <p className='solution'> {solution} </p>
                 <p> You found the solution in {turn} guesses. </p>
-                {/* <a href="http://localhost:3000/">
-                <button type="submit"> PLAY AGAIN?</button>
-                </a> */}
+                <button onClick={() => window.location.reload(false)}> PLAY AGAIN?</button>
             </div>
         )};
 
         {/* modal for if they lose */}
         {!isCorrect && (
             <div>
-                <h1> You have used up all of your guesses</h1>
+                <h2> You have used up all of your guesses</h2>
                 <p className='solution'> The answer was {solution} </p>
                 <p> Better luck next time. </p>
-                {/* <a href="http://localhost:3000/">
-                <button type="submit"> PLAY AGAIN?</button>
-                </a> */}
+                <button onClick={() => window.location.reload(false)}> PLAY AGAIN?</button>
             </div>
         )};
     </div>
