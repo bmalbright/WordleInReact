8e780cc79e478ff69e651c971bdf5b45d04edf6b
Brian Albright <brianalbright@utexas.edu>
2022-07-15 19:46:59 -0500

    styled the modal a little.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -13,8 +13,9 @@ export default function Modal({ isCorrect, turn, solution }) {
             <div>
                 <h2> Congratulations! You Win!</h2>
                 <br/>
-                <p className='solution'> {solution} </p>
-                <p> You found the solution in {turn} guesses. </p>
+                <h2 className='solution'> {solution} </h2>
+                <br/>
+                <h3> You found the solution in {turn} guesses. </h3>
                 <button onClick={() => window.location.reload(false)}> PLAY AGAIN?</button>
             </div>
         )};
@@ -23,8 +24,9 @@ export default function Modal({ isCorrect, turn, solution }) {
         {!isCorrect && (
             <div>
                 <h2> You have used up all of your guesses</h2>
-                <p className='solution'> The answer was {solution} </p>
-                <p> Better luck next time. </p>
+                <h2 className='solution'> The answer was {solution} </h2>
+                <br/>
+                <h3> Better luck next time. </h3>
                 <button onClick={() => window.location.reload(false)}> PLAY AGAIN?</button>
             </div>
         )};
