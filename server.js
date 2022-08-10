c3f34bc35236c28ab84b021b2d4c10e10f77a1a2
Brian Albright <brianalbright@utexas.edu>
2022-08-10 14:27:43 -0500

    trying the server.js one more time.

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-
-const express = require("express");
-const path = require("path");
-require('dotenv').config();
-
-const app = express();
+const jsonServer = require('json-server');
+const app = jsonServer.create();
+const path = require('path');
+const express = require('express');
+const middlewares = jsonServer.defaults();
+const router = jsonServer.router('db.json');
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('public'));
+app.use('/db', middlewares, router);
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.listen(PORT, () => 
-    console.log(`app listening at http://localhost:${PORT}`)
-);
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+server.listen(PORT);
